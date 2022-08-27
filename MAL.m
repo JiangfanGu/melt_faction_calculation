c8c2861c8c2be666f74737c4cf6a652f9d357620
JiangfanGu <112249072+JiangfanGu@users.noreply.github.com>
2022-08-27 18:58:57 +0800

    Rename MCAL.m to MAL.m

--- MAL.m
+++ MAL.m
@@ -1,6 +1,6 @@
 %%Modified archie's law
 %%Ten Grotenhuis S M, Drury M R, Spiers C J, et al. Melt distribution in olivine rocks based on electrical conductivity measurements[J]. Journal of Geophysical Research: Solid Earth, 2005, 110(12): 1-11. https://doi.org/10.1029/2004JB003462.
-%%Watanabe T, Kurita K. The relationship between electrical conductivity and melt fraction in a partially molten simple system: Archie¡¯s law behavior[J]. Physics of the Earth and Planetary Interiors, 1993, 78(1-2): 9-17. https://doi.org/10.1016/0031-9201(93)90079-O.
+%%Watanabe T, Kurita K. The relationship between electrical conductivity and melt fraction in a partially molten simple system: ArchieÂ¡Â¯s law behavior[J]. Physics of the Earth and Planetary Interiors, 1993, 78(1-2): 9-17. https://doi.org/10.1016/0031-9201(93)90079-O.
 %%coded by GuJiangfan@CUGB
 %%meltfraction-conductivity 
 meltsigma=1;%replace it with any result you find/calculate/measure

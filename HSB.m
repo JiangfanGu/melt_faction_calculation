e5f3cfd3796211f8853022055016c9b14e572778
JiangfanGu <112249072+JiangfanGu@users.noreply.github.com>
2022-08-27 18:46:18 +0800

    Update HSB.m

--- HSB.m
+++ HSB.m
@@ -1,11 +1,9 @@
 %%Hashin-Shtrikman Boundery
-%%Hashin Z, Shtrikman S. A Variational approach to the theory of the effective 
-%%magnetic permeability of multiphase materials[J]. Journal of Applied Physics, 
-%%1962, 33(10): 3125-3131. https://doi.org/10.1063/1.1728579.
+%%Hashin Z, Shtrikman S. A Variational approach to the theory of the effective magnetic permeability of multiphase materials[J]. Journal of Applied Physics, 1962, 33(10): 3125-3131. https://doi.org/10.1063/1.1728579.
 %%coded by GuJiangfan@CUGB
 %%meltfraction-conductivity 
-meltsigma=1;
-sigmarock=1000;
+meltsigma=1;%replace it with any result you find/calculate/measure
+sigmarock=1000;%replace it with any result you find/calculate/measure
 phi=0:0.01:1;
 phip=phi*100;
 sigmahsp=meltsigma+(1-phi).*(1./(1/(sigmarock-meltsigma)+phi/meltsigma/3));
